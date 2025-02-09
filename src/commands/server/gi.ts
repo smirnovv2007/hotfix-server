31fd06e55971c57228825a18547028f4c05a94ea
Yuuki <6851027-yukiz@users.noreply.gitlab.com>
2025-02-09 14:04:31 +0800

    add `patch_node_versions` and fix download order

--- src/commands/server/gi.ts
+++ src/commands/server/gi.ts
@@ -9,41 +9,60 @@ export default async function handle(command: Command) {
 	const folderDownload = `/home/node/app/src/server/web/public/cache/game/genshin`
 	const mainUrl = "https://autopatchhk.yuanshen.com"
 
+	// object with the versions sorted in ascending order (make sure the order is correct to avoid 404 download error)
 	const list = {
 		"5.0_live": [
-			{
-				client: { Version: 27173400, Suffix: "b79a0d313d" },
-				clientSilence: { Version: 27370672, Suffix: "da441c29cf" },
-				res: { Version: 26885261, Suffix: "05bfe00485" }
-			}
-			/*
-			{
-				client: { Version: 26487341, Suffix: "57a90bbd52" }
-			},
-			{
-				clientSilence: { Version: 26530839, Suffix: "185bf02ea7" }
-			},
 			{
 				res: { Version: 26161852, Suffix: "4c9dae0f9a" },
 				client: { Version: 26547926, Suffix: "3d43078d67" }
 			},
 			{
-				//res: { Version: 26458901, Suffix: "befdda25ff" },
+				clientSilence: { Version: 26530839, Suffix: "185bf02ea7" }
+			},
+			{
 				clientSilence: { Version: 26594938, Suffix: "e3714681f7" },
 				client: { Version: 26594938, Suffix: "e3714681f7" }
 			},
 			{
-				//res: { Version: 26709834, Suffix: "034791c61b" },
 				clientSilence: { Version: 26720294, Suffix: "79e8f3e212" },
 				client: { Version: 26720294, Suffix: "79e8f3e212" }
+			},
+			{
+				res: { Version: 26885261, Suffix: "05bfe00485" }
+			},
+			{
+				client: { Version: 26487341, Suffix: "57a90bbd52" }
+			},
+			{
+				client: { Version: 27173400, Suffix: "b79a0d313d" },
+				clientSilence: { Version: 27370672, Suffix: "da441c29cf" }
 			}
-            */
 		],
 		"5.3_live": [
+			{
+				clientSilence: { Version: 30268382, Suffix: "0ec19ee5e5" }
+			},
+			{
+				client: { Version: 30106004, Suffix: "8a4a5364c3" },
+				res: { Version: 30064194, Suffix: "3e5c6aef48" }
+			},
 			{
 				client: { Version: 29858484, Suffix: "07afd11c59" },
 				clientSilence: { Version: 29858484, Suffix: "07afd11c59" },
 				res: { Version: 29828689, Suffix: "f49ab1b204" }
+			},
+			{
+				client: { Version: 29707439, Suffix: "9d14e2dd86" },
+				clientSilence: { Version: 29707439, Suffix: "9d14e2dd86" },
+				res: { Version: 29705774, Suffix: "7e218277ba" }
+			},
+			{
+				client: { Version: 29634921, Suffix: "664041d3aa" },
+				res: { Version: 29500234, Suffix: "844688026e" }
+			},
+			{
+				clientSilence: { Version: 29549132, Suffix: "c32325d9d4" },
+				res: { Version: 29332470, Suffix: "03eda7a0ec" }
 			}
 		]
 	}
@@ -54,15 +73,20 @@ export default async function handle(command: Command) {
 			// 'client/Android', 'client/StandaloneWindows64', 'client/iOS', 'client/PS5', 'client/PS4'
 			Clients: ["client/Android", "client/StandaloneWindows64", "client/iOS"],
 			Mappers: [
+				// res files
 				"res_versions_external",
 				"res_versions_medium",
 				"res_versions_streaming",
 				"release_res_versions_external",
 				"release_res_versions_medium",
 				"release_res_versions_streaming",
+				// audio files
+				"AudioAssets/audio_versions",
+				// basic files
 				"base_revision",
 				"script_version",
-				"AudioAssets/audio_versions"
+				"patch_node_versions", // NEW, I don't know since when but the file might be useful to check which files need to be downloaded in this hotfix version
+				"vulkan_gpu_list_config.txt" // android only?
 			]
 		},
 		clientSilence: {
@@ -91,25 +115,41 @@ export default async function handle(command: Command) {
 						const saveFilePath = `${saveFileFolder}/${mapper}`
 
 						// Check if URL is cached
+                        /*
 						if (cache[mapperUrl]) {
-							log.warn(`Skipping: ${mapperUrl} (Cached)`)
+							log.info(`Skipping: ${mapperUrl} (Cached 1)`)
+							continue
+						}
+                        */
+
+						// download mapper file
+						var isSave0 = await forceDownload(mapperUrl, saveFilePath)
+						if (isSave0 != 0) {
+							log.errorNoStack(`Download failed ${mapperUrl} > ${saveFilePath}`)
 							continue
 						}
 
-						await forceDownload(mapperUrl, saveFilePath)
+						// Not a download list file
+						if (
+							[
+								"script_version",
+								"base_revision",
+								"patch_node_versions",
+								"vulkan_gpu_list_config.txt"
+							].includes(mapper)
+						)
+							continue
 
-						if (["script_version", "base_revision"].includes(mapper)) continue
-
+						// read mapper file
 						let fileContent = ""
 						try {
 							fileContent = await fs.readFile(saveFilePath, "utf-8")
 						} catch (error) {
-							log.warn(`File not found: ${saveFilePath}`)
+							log.errorNoStack(`File not found: ${saveFilePath}`)
 							continue
 						}
-
+						// read mapper file line by line
 						const mapperLines = fileContent.split("\n")
-
 						for (const line of mapperLines) {
 							if (!line) continue
 							const mapperData = getMd5Data(line)
@@ -136,25 +176,61 @@ export default async function handle(command: Command) {
 									`${fileFolder}/`
 								)
 
+							// Check if URL is cached aka done downloading
 							if (cache[gameFileUrl]) {
-								log.warn(`Skipping: ${gameFileUrl} (Cached)`)
+								log.info(`Skipping: ${gameFileUrl} > ${cache[gameFileUrl]} (Cached 2)`)
 								continue
 							}
 
-							// Check MD5
 							const expectedMd5 = mapperData.md5 || ""
+
+							// Not all files are in the current version, sometimes they are only in the previous version, so to prevent downloading files not found or duplicate download, we make sure that the file already exists in the previous version by checking md5 file only without use output
+							const normalizePath = (url: string) => {
+								return url
+									.replace(mainUrl + "/", "") // Remove base URL
+									.replace(/output_\d+_[a-f0-9]+\//, "") // Remove `output_<version>_<suffix>/`
+							}
+							const filePath = normalizePath(gameFileUrl)
+							// Find cache entry, ignoring the version folder
+							const cachedEntry = Object.entries(cache).find(([cachedUrl, cachedMd5]) => {
+								return normalizePath(cachedUrl) === filePath
+							})
+							if (cachedEntry) {
+								const [cachedUrl, cachedMd5] = cachedEntry
+
+								if (cachedMd5 === expectedMd5) {
+									log.info(`Skipping: ${gameFileUrl} > ${cachedUrl} (Cached and MD5 matched)`)
+									continue
+								} else {
+									if (cachedMd5 === "not_found") {
+										log.warn(`Skipping: ${gameFileUrl} > ${cachedUrl} (Cached and not found)`)
+										continue
+									}
+									log.errorNoStack(
+										`MD5 mismatch for cached file: ${cachedUrl} -> Expected: ${expectedMd5}, Found: ${cachedMd5}`
+									)
+								}
+							}
+
+							// Check MD5 file
 							const isValid = await getMD5HashFile(gameFileSavePath, expectedMd5)
 							if (!isValid) {
 								log.warn(`MD5 mismatch > ${gameFileSavePath} > try download ${gameFileUrl}`)
 								var isSave = await forceDownload(gameFileUrl, gameFileSavePath)
-								if (isSave) {
+								if (isSave == 0) {
 									const isValid2 = await getMD5HashFile(gameFileSavePath, expectedMd5)
 									if (isValid2) {
 										log.info(`Download done and vaild > ${gameFileSavePath}`)
 										cache[gameFileUrl] = expectedMd5
 										await saveCache(CACHE_FILE, cache)
 									} else {
-										log.warn(`Download done but not vaild > ${gameFileSavePath}`)
+										log.errorNoStack(`Download done but not vaild > ${gameFileSavePath}`)
+									}
+								} else {
+									log.errorNoStack(`Download failed > ${gameFileSavePath}`)
+									if (isSave == -1) {
+										cache[gameFileUrl] = "not_found"
+										await saveCache(CACHE_FILE, cache)
 									}
 								}
 							} else {
